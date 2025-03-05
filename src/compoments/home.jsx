9e433c622d25e3bf937caad6a3c54a3f5f3cb71f
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-03-05 18:33:39 +0100

    Build: set up home page

---
+++ src/compoments/home.jsx
@@ -0,0 +1,18 @@
+import "../styles/home.css"
+function Home(){
+
+    return(
+        <section className="home">
+            <div className="img-container">
+                <img src="/imgs/tiramisu.jpeg" alt="" className="header-pic" />
+            </div>
+            <div className="intro">
+                <h1 className="quote">Cooking Inspirations, Family Recipes and Shared Joy</h1>
+                <h2 className="title">Food as A Love Langauge</h2>
+                <p>Hi ya! You've found a digital corner that keeps all of my receipes. I am a big foodie, but more than that, I am love cooking. I belive to cook is to care. So, check around! </p>
+            </div>
+        </section>
+    )
+}
+
+export default Home
