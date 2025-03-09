7fe6f9665f1ba0bc0a3c8bb1645075b41713d848
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-03-09 21:33:26 +0100

    Build: add middleware to receive image file

--- server/controllers/reciepeRouter.js
+++ server/controllers/reciepeRouter.js
@@ -1,14 +1,29 @@
 // set up router to receive CRUD requests for reciepe
 const { Router } = require("express")
 const reciepeRouter = new Router()
+// use Multer middleware to recieve img file
+const multer = require("multer")
+const path = require("path")
 const db = require("../models/queries")
 
+// set up image file storage 
+const storage = multer.diskStorage({
+    destination:function(req, file, cb){
+        cb(null, "uploads/")
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname + path.extname(file.originalname))
+    }
+})
+const upload = multer({storage:storage})
 
 // create new reciepe
-reciepeRouter.post("/", async(req,res)=>{
+reciepeRouter.post("/", upload.single("image"), (req,res)=>{
     console.log("post request recieved to create new reciepe");
-    console.log(req.body)
-    await db.addReciepe(req.body) 
+    console.log(JSON.parse(req.body.data))
+    console.log(req.file)
+    // const entry = JSON.parse(req.body.data)
+    // await db.addReciepe(entry) 
     res.json();
 })
 
@@ -18,7 +33,7 @@ reciepeRouter.get("/", async(req,res)=>{
     res.json(result)
 })
 
-reciepeRouter.put("/", (req,res)=>{
+reciepeRouter.put("/",(req,res)=>{
     console.log("request recieved to update reciepe");
     console.log(req.body)
     res.json()
